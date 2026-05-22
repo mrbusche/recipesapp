be7180c9fe52708a1798daec355e46db27631350
Matt Busche <mrbusche@gmail.com>
2026-05-22 17:15:27 -0500

    fix rss update

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const { DateTime } = require('luxon');
 const fs = require('fs');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
+const rssPluginImport = require('@11ty/eleventy-plugin-rss');
+const pluginRss = rssPluginImport.default || rssPluginImport;
 const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginNavigation = require('@11ty/eleventy-navigation');
 const markdownIt = require('markdown-it');
